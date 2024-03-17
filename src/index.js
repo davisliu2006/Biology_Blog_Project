cf136f8b705178d7ec932c54dd35fd8e3512603f
Davis Liu <davisliu2006@gmail.com>
2024-03-17 13:13:30 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-let secBar = document.getElementById("secondary-bar");
-let secBarY = 60;
 let scrY0 = window.scrollY;
+let scrY = window.scrollY;
+let secBar = document.getElementById("secondary-bar");
+let secBarY = 60; // px
 window.onscroll = function() {
-    let scrY = window.scrollY;
-    if (scrY > window.visualViewport.height) {
+    let vh = window.visualViewport.height;
+    scrY = window.scrollY;
+    let dScrY = scrY-scrY0;
+
+    // secondary bar
+    if (scrY > vh) {
         secBar.style.position = "fixed";
         secBar.style.top = "60px";
-        if (scrY > scrY0) {
-            secBarY = Math.max(10, secBarY -= (scrY-scrY0)/5);
+        if (dScrY > 0) {
+            secBarY = Math.max(10, secBarY -= dScrY/5);
         } else {
-            secBarY = Math.min(60, secBarY -= (scrY-scrY0)/5);
+            secBarY = Math.min(60, secBarY -= dScrY/5);
         }
         secBar.style.top = secBarY+"px";
     } else {
         secBar.style.position = "absolute";
         secBar.style.top = "100vh";
     }
+
     scrY0 = scrY;
 }
